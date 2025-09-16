fc3c199e61996f7340fea1c89cb4a3fe949dee4b
BensaltanaHoussam <bensaltna07@gmail.com>
2025-09-16 16:51:14 +0100

    implementer les methode retirer et calculerInteret est afficherDetails dans la class Compte Epargne

--- src/banque/CompteEpargne.java
+++ src/banque/CompteEpargne.java
@@ -1,4 +1,32 @@
 package banque;
 
-class CompteEpargne {
+class CompteEpargne extends Compte {
+    private double tauxInteret =0.03;
+
+    CompteEpargne(String code, double solde, double tauxInteret) {
+        super (code, solde);
+        this.tauxInteret = tauxInteret ;
+    }
+
+    @Override
+    public void retirer(double montant) {
+        if (montant <= 0) {
+            throw new IllegalArgumentException("Montant invalide : doit être positif.");
+        }
+        if (montant <= solde) {
+            solde -= montant;
+        } else {
+            throw new IllegalArgumentException("Retrait refusé : fonds insuffisants.");
+        }
+    }
+
+    @Override
+    public double calculerInteret() {
+        return solde * tauxInteret;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Compte Epargne: " + code + ", Solde: " + solde + ", Taux d'intérêt: " + tauxInteret);
+    }
 }
