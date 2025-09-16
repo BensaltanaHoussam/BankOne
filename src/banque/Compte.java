1df08d085d9456e3dd853a604e950d7fb10b7aca
BensaltanaHoussam <bensaltna07@gmail.com>
2025-09-16 16:41:49 +0100

    implementer les methode retirer et calculerInteret dans la class Compte Courant

--- src/banque/Compte.java
+++ src/banque/Compte.java
@@ -1,15 +1,37 @@
 package banque;
-import com.sun.corba.se.spi.orb.Operation;
-import java.util.List;
 
+import com.sun.corba.se.spi.orb.Operation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 abstract class Compte {
     protected String code;
     protected double solde;
-    protected List<Operation> listOperations;
+    protected ArrayList<Operation> listeOperations;
+
+    public Compte(String code, double solde) {
+        if (!code.matches("CPT-\\d{5}")) {
+            throw new IllegalArgumentException("Code invalide : format CPT-XXXXX requis.");
+        }
+        this.code = code;
+        this.solde = solde;
+        this.listeOperations = new ArrayList<>();
+    }
 
     public abstract void retirer(double montant);
     public abstract double calculerInteret();
     public abstract void afficherDetails();
 
+    public String getCode() {
+        return code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public List<Operation> getListeOperations() {
+        return listeOperations;
+    }
 }
--- src/banque/CompteCourant.java
+++ src/banque/CompteCourant.java
@@ -1,4 +1,37 @@
 package banque;
 
-public class CompteCourant {
+public class CompteCourant extends Compte {
+
+    private double decouvert = 5000.0;
+
+    CompteCourant(String code, double solde) {
+        super(code, solde);
+    }
+
+    @Override
+    public void retirer(double montant) {
+        if (montant <= 0) {
+            throw new IllegalArgumentException("Montant invalide : doit être positif.");
+        }
+        if (solde - montant >= -decouvert) {
+            solde -= montant;
+        } else {
+            throw new IllegalArgumentException("Retrait refusé : dépassement du découvert autorisé.");
+        }
+
+
+    }
+
+    @Override
+    public double calculerInteret() {
+        return 0;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Compte Courant: " + code + ", Solde: " + solde + ", Découvert: " + decouvert);
+    }
+
+
+
 }
